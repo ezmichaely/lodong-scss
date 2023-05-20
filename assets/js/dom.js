14d5f9322a744c3be63591c7874d3dd6a5851f2a
EZ MICHAEL YUCOR <ezmichaely@gmail.com>
2023-05-20 11:08:47 +0800

    not done yet with services

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,10 +1,13 @@
-// scroll to Top
-function scrollToTop () {
-    document.addEventListener('DOMContentLoaded', function () {
-        let scrollBtn = document.querySelector("#scrollTop");
-    
-        scrollBtn.addEventListener('click', function () {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        });
-    }, false);
-}
+const menuBtn = document.querySelector("#menuBtn");
+const menuNav = document.querySelector("#menuNav");
+const menuClose = document.querySelector("#menuClose");
+
+menuBtn.addEventListener('click', function () {
+    menuNav.classList.toggle('open');
+    document.querySelector('body').classList.add('overflow-y-hidden');
+});
+
+menuClose.addEventListener('click', function () {
+    menuNav.classList.toggle('open');
+    document.querySelector('body').classList.remove('overflow-y-hidden');
+});
--- assets/js/function.js
+++ assets/js/function.js
@@ -8,3 +8,37 @@ function scrollToTop() {
         });
     }, false);
 }
+
+
+function openNextPageOnScrollDown(page) {
+    // Specify the threshold percentage when to trigger the page transition
+    const scrollThreshold = 0.8;
+
+    // Add an event listener to detect mouse wheel scrolling
+    window.addEventListener('wheel', (event) => {
+        // Check if the user has scrolled beyond the threshold
+        if (Math.abs(event.deltaY) >= scrollThreshold) {
+            // Perform your desired action, such as redirecting to another page
+            window.location.href = page;
+        }
+    });
+}
+
+function openNextPageOnScrollUp(page) {
+    let isAtTop = true;
+
+    window.addEventListener('wheel', (event) => {
+        if (isAtTop && event.deltaY < 0) {
+            // Perform your desired action, such as redirecting to another page
+            window.location.href = page;
+        } else {
+            isAtTop = false;
+        }
+    });
+
+    // Add an event listener to detect scroll position on page load
+    window.addEventListener('scroll', () => {
+        isAtTop = window.scrollY === 0;
+    });
+}
+

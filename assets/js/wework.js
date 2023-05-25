3c2b383cf7f0f7361de3e22a2b0ad11b94881cde
EZ MICHAEL YUCOR <ezmichaely@gmail.com>
2023-05-25 20:16:53 +0800

    update

---
+++ assets/js/wework.js
@@ -0,0 +1,70 @@
+function visSection(section) {
+    section.find('.left').addClass("animate__animated animate__bounceInLeft");
+    section.find('.right').addClass("animate__animated animate__bounceInRight");
+}
+
+function hidSection(section) {
+    section.find('.aniIn_bounceInLeft').removeClass("animate__animated animate__bounceInLeft");
+    section.find('.aniIn_bounceInRight').removeClass("animate__animated animate__bounceInRight");
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#weworkPage').classList.add('slideUp');
+    scrollToTop();
+
+    if (window.innerWidth > 1025) {
+        const page = './index.html';
+        openNextPageOnScrollUp(page);
+    }
+})
+
+// $(window).scroll(function () {
+//     const scrollPosition = $(window).scrollTop();
+//     const windowHeight = $(window).height();
+
+//     $('section').each(function () {
+//         const elementOffset = $(this).offset().top;
+//         if (scrollPosition > elementOffset - windowHeight + 50) {
+//             $(this).removeClass('opacity-0');
+//             $(this).find('.right').addClass('animate__animated animate__bounceInRight');
+//             $(this).find('.left').addClass('animate__animated animate__bounceInLeft');
+//         }
+
+//         else {
+//             $(this).addClass('opacity-0');
+//             $(this).find('.right').removeClass('animate__animated animate__bounceInRight');
+//             $(this).find('.left').removeClass('animate__animated animate__bounceInLeft');
+//         }
+//     });
+// });
+
+let animatedElements = [];
+
+$(window).scroll(function () {
+    const scrollPosition = $(window).scrollTop();
+    const windowHeight = $(window).height();
+
+    $('section').each(function () {
+        const elementOffset = $(this).offset().top;
+        const elementId = $(this).attr('id');
+
+        if (scrollPosition > elementOffset - windowHeight + 200) {
+            if (!animatedElements.includes(elementId)) {
+                $(this).removeClass('opacity-0');
+                $(this).find('.right').addClass('animate__animated animate__bounceInRight');
+                $(this).find('.left').addClass('animate__animated animate__bounceInLeft');
+                animatedElements.push(elementId);
+            }
+        } else {
+            if (animatedElements.includes(elementId)) {
+                $(this).addClass('opacity-0');
+                $(this).find('.right').removeClass('animate__animated animate__bounceInRight');
+                $(this).find('.left').removeClass('animate__animated animate__bounceInLeft');
+                animatedElements = animatedElements.filter(function (item) {
+                    return item !== elementId;
+                });
+            }
+        }
+    });
+});
